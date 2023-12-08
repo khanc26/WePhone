e57f1a6669a03fc5700213a3158ce3f7c5c04c3f
KhaProMax <khaqt@gmail.com>
2023-12-08 16:33:36 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace WePhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["CurrentTab"] = "Index";
+            return View();
+        }
+
+        public IActionResult UserCRUD()
+        {
+			ViewData["CurrentTab"] = "UserCRUD";
+			return View();
+        }
+
+		public IActionResult Category()
+		{
+            //var users = _context.User.ToList();
+
+            //// Log the car features to the console
+            //foreach (var user in users)
+            //{
+            //    _logger.LogInformation($"User - Id: {user.User_Id}, FeatureName: {user.First_Name}");
+            //}
+
+            //return Ok(); // Optionally, you can return an IActionResult indicating success
+            return View();
+        }
+
+		public IActionResult Profile()
+		{
+			ViewData["CurrentTab"] = "Profile";
+			return View();
+		}
+
+		public IActionResult SignUp()
+		{
+			ViewData["CurrentTab"] = "SignUp";
+			return View();
+		}
+
+		public IActionResult SignIn()
+		{
+			ViewData["CurrentTab"] = "SignIn";
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using WePhone.Models;
+global using WePhone.Data;
